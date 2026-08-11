7bc9d8ea6928f486f5bc1b1f31165b3f9fb4fbd4
Findya <arlikha06@gmail.com>
2026-08-11 14:03:56 +0700

    fix: Ensure HTTPS scheme is enforced in production environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (app()->environment('production')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+        
         Asset::observe(AssetObserver::class);
     }
 }

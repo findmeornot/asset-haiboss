6506ef99bcc83d870051366522cd2c309fd7b8bb
Findya <arlikha06@gmail.com>
2026-08-11 15:52:28 +0700

    fix: Update welcome redirect to admin login with success message

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Services\BeritaAcaraService;
 use App\Http\Controllers\ReportController;
 
 Route::get('/', function () {
-    return redirect()->route('admin.login')->with('success', 'Selamat datang di Aplikasi Asset Management!');
+    return redirect('/admin');
 });
 
 Route::get('/asset/{id}/print-label', function ($id) {

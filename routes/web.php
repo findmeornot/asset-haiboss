9810b2d6e78c1304ce1ad87afb219ed094829299
Findya <arlikha06@gmail.com>
2026-08-11 11:23:01 +0700

    feat: Update AdminPanelProvider path and remove welcome route from web.php

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Route::get('/asset/{id}/print-label', function ($id) {
     abort_unless(auth()->check(), 403);
